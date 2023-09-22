75568dc8e1ac0b891edc3868314645df78c7e2c8
KjetilBerg <kjetil.a.berg@hotmail.com>
2023-09-22 17:23:13 +0200

    part 2 - tests passing

--- src/main/java/no/hvl/dat250/rest/todos/TodoController.java
+++ src/main/java/no/hvl/dat250/rest/todos/TodoController.java
@@ -1,9 +1,47 @@
 package no.hvl.dat250.rest.todos;
 
-/**
- * Rest-Endpoint for todos.
- */
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+/** Rest-Endpoint for todos. */
+@RestController
 public class TodoController {
 
   public static final String TODO_WITH_THE_ID_X_NOT_FOUND = "Todo with the id %s not found!";
+  private HashMap<Integer, Todo> todos = new HashMap<>();
+  private Long currentId = 0L;
+
+  @PostMapping("/todos")
+  public Todo postTodo(@RequestBody Todo todo) {
+    todo.setId(currentId++);
+    todos.put(todo.getId().intValue(), todo);
+    return todo;
+  }
+
+  @GetMapping("/todos")
+  public Collection<Todo> getTodos() { return todos.values(); }
+
+  @GetMapping("/todos/{id}")
+  public Todo getTodo(@PathVariable int id) {
+    if (!todos.containsKey(id)) throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format(TODO_WITH_THE_ID_X_NOT_FOUND, id));
+    return todos.get(id);
+  }
+
+  @PutMapping("/todos/{id}")
+  public void updateTodo(@PathVariable int id, @RequestBody Todo todo) {
+    todo.setId((long)id);
+    todos.put(id, todo);
+  }
+
+  @DeleteMapping("/todos/{id}")
+  public void deleteTodo(@PathVariable int id) {
+    if (!todos.containsKey(id)) throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format(TODO_WITH_THE_ID_X_NOT_FOUND, id));
+    todos.remove(id);
+  }
+
 }
